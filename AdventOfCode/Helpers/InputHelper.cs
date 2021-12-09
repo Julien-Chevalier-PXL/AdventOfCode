608fba8ba3a308eebf405f2f67f46b832ac9cdf9
Julien C <25689385+Julien-Chevalier-PXL@users.noreply.github.com>
2021-12-09 21:33:56 +0100

    [AoC 2021] InputHelper - Copy Inputs directory to outputdirectory
    Easier access to input files by copying the 'Inputs' folder to the output directory on build

--- AdventOfCode/Helpers/InputHelper.cs
+++ AdventOfCode/Helpers/InputHelper.cs
@@ -5,13 +5,13 @@
 
     public static class InputHelper
     {
-        public const string INPUT_FILE_LOCATION = @"..\..\..\Inputs\";
+        public const string INPUT_FOLDER_LOCATION = @"Inputs";
         public const string INPUT_FILE = "{0}.txt";
         public const string SMALL_INPUT_FILE = "{0}Small.txt";
         public const string TEST_INPUT_FILE = "{0}Test.txt";
 
-        public static IEnumerable<string> GetInput(string day) => File.ReadAllLines(Path.Combine(INPUT_FILE_LOCATION, string.Format(INPUT_FILE, day)));
-        public static IEnumerable<string> GetSmallInput(string day) => File.ReadAllLines(Path.Combine(INPUT_FILE_LOCATION, string.Format(SMALL_INPUT_FILE, day)));
-        public static IEnumerable<string> GetTestInput(string day) => File.ReadAllLines(Path.Combine(INPUT_FILE_LOCATION, string.Format(TEST_INPUT_FILE, day)));
+        public static IEnumerable<string> GetInput(string day) => File.ReadAllLines(Path.Combine(INPUT_FOLDER_LOCATION, string.Format(INPUT_FILE, day)));
+        public static IEnumerable<string> GetSmallInput(string day) => File.ReadAllLines(Path.Combine(INPUT_FOLDER_LOCATION, string.Format(SMALL_INPUT_FILE, day)));
+        public static IEnumerable<string> GetTestInput(string day) => File.ReadAllLines(Path.Combine(INPUT_FOLDER_LOCATION, string.Format(TEST_INPUT_FILE, day)));
     }
 }
